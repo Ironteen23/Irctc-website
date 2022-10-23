ab6898502cb637610102bfb8072e082fb7cc296d
Hem <hpmahimkar@gmail.com>
2022-10-23 16:26:55 +0530

    Train crud functions added

--- server/controllers/trains.js
+++ server/controllers/trains.js
@@ -1,19 +1,43 @@
-const getAllTrains = (req, res) => {
-  res.send("all trains lol");
+const Train = require("../models/trains");
+
+const getAllTrains = async (req, res) => {
+  try {
+    const trains = await Train.find({});
+    res.status(200).json({ trains: trains });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
-const createTrain = (req, res) => {
-  res.json(req.body);
+const createTrain = async (req, res) => {
+  try {
+    const train = await Train.create(req.body);
+    res.status(201).json({ train });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const updateTrain = (req, res) => {
   res.send("update train");
 };
 
-const getTrain = (req, res) => {
+const getTrain = async (req, res) => {
+  try {
+    const { id: trainSource } = req.params;
+    const train = await Train.findOne({ source: trainSource });
+
+    if (!train) {
+      return res.status(404).json({ msg: `No trains Found` });
+    }
+    res.status(200).json({ train });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+
   res.json({ id: req.params.id });
 };
-// will give the specific train
+// will give the specific train currently gives train by source destination
 
 const deleteTrain = (req, res) => {
   res.send("delete train");
--- server/models/trains.js
+++ server/models/trains.js
@@ -1,10 +1,48 @@
 const mongoose = require("mongoose");
 
 const TrainSchema = new mongoose.Schema({
-  name: String,
-  source: String,
-  destination: String,
-  distance: Number,
-  TotalSeats: Number,
-  AvailableSeats: Number,
+  name: {
+    type: String,
+    requiured: [true, "Must provide a name"],
+    trim: true,
+  },
+  depDate: {
+    type: Date,
+    requiured: [true, "Must provide a name"],
+    trim: true,
+  },
+  arrDate: {
+    type: Date,
+    requiured: [true, "Must provide a name"],
+    trim: true,
+  },
+  source: {
+    type: String,
+    requiured: [true, "Must provide a source"],
+    trim: true,
+  },
+  destination: {
+    type: String,
+    requiured: [true, "Must provide a destination"],
+    trim: true,
+  },
+  distance: {
+    type: String,
+    requiured: [true, "Must provide the distance"],
+    trim: true,
+  },
+  // TotalSeats: Number,
+  // AvailableSeats: Number,
+  depatureTime: {
+    type: String,
+    requiured: [true, "Must provide a depatureTime"],
+    trim: true,
+  },
+  arrivalTime: {
+    type: String,
+    requiured: [true, "Must provide a arrivalTime"],
+    trim: true,
+  },
 });
+
+module.exports = mongoose.model("Train", TrainSchema);
