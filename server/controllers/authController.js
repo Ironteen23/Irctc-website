ab6e4185d682d0d08f47009e2c601d560f50a55a
Hem <hpmahimkar@gmail.com>
2022-10-31 20:08:36 +0530

    admin view all users f(x) added

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -5,7 +5,7 @@ const User = require("../models/User");
 const getAllUsers = async (req, res) => {
   try {
     const users = await User.find({});
-    res.status(200).json({ users: users });
+    res.status(200).json({ users });
   } catch (error) {
     res.status(500).json({ msg: error });
   }
